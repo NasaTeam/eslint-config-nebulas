05312d158146a8be70b66eb1222b81fb754360a1
newraina <newraina@users.noreply.github.com>
2018-08-30 19:25:52 +0800

    add new rules from updated eslint-plugin-nebulas

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ module.exports = {
   rules: {
     'nebulas/export': 'error',
     'nebulas/export-contract': 'error',
+    'nebulas/limited-libs': 'error',
+    'nebulas/no-esm': 'error',
     'no-undef': 'error'
   }
 }
